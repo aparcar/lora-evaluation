f02528166af48fdeaa7e6de2719bd6dc76bd1909
Paul Spooren <mail@aparcar.org>
2020-04-23 22:34:27 -1000

    json2influx: add config
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- json2influx/json2influx.py
+++ json2influx/json2influx.py
@@ -4,17 +4,29 @@ import logging
 import sys
 from influxdb import InfluxDBClient
 from influxdb import SeriesHelper
+from configparser import ConfigParser
+
+config = ConfigParser(allow_no_value=True)
+config.read("config.ini")
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=config["main"]["logging_level"],
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[logging.FileHandler("debug.log"), logging.StreamHandler(sys.stderr)],
 )
 
-client = InfluxDBClient("localhost", 8086, "root", "root", "uhmdemo")
-client.create_database("uhmdemo")
+client = InfluxDBClient(
+    config["influx"]["host"],
+    config["influx"]["port"],
+    config["influx"]["user"],
+    config["influx"]["pass"],
+    config["influx"]["database"],
+)
+client.create_database(config["influx"]["database"])
 
 nodes = {}
+for node in config["nodes"].keys():
+    nodes[node] = 0
 
 
 class BasicSeriesHelper(SeriesHelper):
@@ -46,17 +58,22 @@ while True:
 
         node = msg.pop("node", None)
         if not node:
-            logging.error(f"node attribute missing in '{line}")
+            logging.error(f"node attribute missing in '{line}'")
             continue
 
-        if node in nodes:
-            msg_cnt_diff = msg.get("cnt", 0) - nodes[node].get("cnt", 0)
-            if msg_cnt_diff > 1:
-                logging.warning(f"missed {msg_cnt_diff} message(s) from {node}")
-            elif msg_cnt_diff < 1:
-                logging.warning(f"missed {msg_cnt_diff} message(s) from {node}")
+        if node not in nodes:
+            logging.info(f"skip unknown node {node}")
+            continue
 
-        nodes[node] = msg
+        current_cnt = msg.get("cnt", 0) or 0
+
+        msg_cnt_diff = current_cnt - nodes.get(node, 0)
+        if msg_cnt_diff > 1:
+            logging.warning(f"missed {msg_cnt_diff} message(s) from {node}")
+        elif msg_cnt_diff < 1:
+            logging.warning(f"missed {msg_cnt_diff} message(s) from {node}")
+
+        nodes[node] = current_cnt
 
         BasicSeriesHelper(
             gateway="feather_gw",
